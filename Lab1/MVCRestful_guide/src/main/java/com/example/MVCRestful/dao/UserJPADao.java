51098238c365bb092f0c486c7665fe6392f41fe3
YountenTshering <77493302+YountenTshering@users.noreply.github.com>
2021-01-29 12:19:04 +0700

    deleted file

--- Lab1/MVCRestful_guide/src/main/java/com/example/MVCRestful/dao/UserJPADao.java
+++
@@ -1,9 +0,0 @@
-package com.example.MVCRestful.dao;
-
-import com.example.MVCRestful.model.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserJPADao extends JpaRepository<User, Integer> {
-    
-}
--- Lab1/MVCRestful_guide/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into user values (101, 'Younten', 'Bhutan');
-insert into user values (102, 'Sam', 'England');
-insert into user values (103, 'Jo', 'Japan');
-insert into user values (104, 'Thinley', 'China');
