57a0b596455ef70896250a4fe82dbcec21fbdbf5
YountenTshering <77493302+YountenTshering@users.noreply.github.com>
2021-01-29 12:04:34 +0700

    Change the name of folder

---
+++ Lab1_Finish the guide/MVCRestful_guide/src/main/java/com/example/MVCRestful/MvcRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.MVCRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcRestfulApplication.class, args);
+	}
+
+}
---
+++ Lab1_Finish the guide/MVCRestful_guide/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values (101, 'Younten', 'Bhutan');
+insert into user values (102, 'Sam', 'England');
+insert into user values (103, 'Jo', 'Japan');
+insert into user values (104, 'Thinley', 'China');
