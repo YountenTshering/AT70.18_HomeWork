0ba616dd320460060fe214983593c3a3ca769de9
YountenTshering <77493302+YountenTshering@users.noreply.github.com>
2021-02-23 18:04:30 +0700

    Upload pic and update readme file

--- Lab3_guide/ORM/src/main/resources/data.sql
+++ Lab3_guide/ORM/src/main/resources/data.sql
@@ -1,8 +1,8 @@
-insert into user (username, password, active, role) values ('chaklam', '$2y$12$2LA4/IzwsfoF.SFtdxwJIus48N6JwFzTdMwlrc9lXRHnA9EOBU7AS', true, 'ROLE_ADMIN');
-insert into user (username, password,  active, role) values ('john', '$2y$10$uwslZFS.czXR6VE7XzyBTuH.xGtdDmBBnioj2J/KZqJr0cHDEr3fa', true, 'ROLE_USER');
-insert into user (username, password,  active, role) values ('peter', '$2y$10$uwslZFS.czXR6VE7XzyBTuH.xGtdDmBBnioj2J/KZqJr0cHDEr3fa', true, 'ROLE_USER');
-insert into employee_info (employee_age, fname, lname, mname, user_id) values (20, 'Chaklam', 'Silpasuwanchai', ' ', 1);
-insert into employee_info (employee_age, fname, lname, mname, user_id) values (69, 'John', 'Cahill', ' ', 2);
+insert into user (username, password, active, role) values ('Younten', '$2y$12$2LA4/IzwsfoF.SFtdxwJIus48N6JwFzTdMwlrc9lXRHnA9EOBU7AS', true, 'ROLE_ADMIN');
+insert into user (username, password,  active, role) values ('Jams', '$2y$10$uwslZFS.czXR6VE7XzyBTuH.xGtdDmBBnioj2J/KZqJr0cHDEr3fa', true, 'ROLE_USER');
+insert into user (username, password,  active, role) values ('Kims', '$2y$10$uwslZFS.czXR6VE7XzyBTuH.xGtdDmBBnioj2J/KZqJr0cHDEr3fa', true, 'ROLE_USER');
+insert into employee_info (employee_age, fname, lname, mname, user_id) values (20, 'Younten', 'Silpasuwanchai', ' ', 1);
+insert into employee_info (employee_age, fname, lname, mname, user_id) values (69, 'Yoyo', 'Cahill', ' ', 2);
 insert into benefit(title) values ('Benefit Free Water');
 insert into benefit(title) values ('Benefit Free Coffee');
 insert into employee_info_benefits (emp_user_id, benefits_id) values (1, 1);
